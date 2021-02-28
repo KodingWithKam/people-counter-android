1fd92bc95bad4f01fd9260cae384f3c78019751e
khayes <khayes@hothouseinc.com>
2021-02-27 19:46:37 -0500

    add counter functionality

--- app/src/main/java/com/example/peoplecounter/MainActivity.kt
+++ app/src/main/java/com/example/peoplecounter/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.peoplecounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btnClickMe = findViewById<Button>(R.id.button)
+        val btnText = findViewById<TextView>(R.id.textView)
+        var timesClicked = 0
+        // set on-click listener
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            btnText.text = timesClicked.toString()
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
